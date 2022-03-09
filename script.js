fbc635ee2df1ec401f027c9eb9c18efda2c2e564
Charlie Pines <charlesbec8@gmail.com>
2022-03-09 13:50:24 -0600

    added basic functions

--- script.js
+++ script.js
@@ -20,4 +20,47 @@ function writePassword () {
     writePassword();
   } else
     console.log(passLength)
+    upperCharas();
+}
+
+function upperCharas () {
+  var uppercase = window.confirm ("Include upper case characters?")
+  if (uppercase) {
+    console.log("include uppercase characters")
+    lowerCharas();
+  } else {
+    console.log("no uppercase characters")
+    lowerCharas ();
+  }
+}
+
+function lowerCharas () {
+  var lowercase = window.confirm ("Include lower case characters?")
+  if (lowercase) {
+    console.log("include lowercase characters")
+    numeric ();
+  } else {
+    console.log("no lower case characters")
+    numberic ();
+  }
+}
+
+function numeric () {
+  var numbers = window.confirm ("Include numbers?")
+  if (numbers) {
+    console.log("include numbers")
+    special ();
+  } else {
+    console.log("no numbers")
+    special ();
+  }
+}
+
+function special () {
+  var specialCharas = window.confirm ("Include special characters?")
+  if (specialCharas) {
+    console.log("include special characters")
+  } else {
+    console.log("no special characters")
+  }
 }
