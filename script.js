1c75fc5dc92252f77a8a7933b6d0589a33149f52
Charlie Pines <charlesbec8@gmail.com>
2022-03-11 14:20:46 -0600

    updated functions and added arrays

--- script.js
+++ script.js
@@ -13,75 +13,87 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function writePassword () {
+var password = ""
+
+function generatePassword () {
+ var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+ var numerals = [1,2,3,4,5,6,7,8,9,0];
+ var specials = ["!","?","#","$","&","^","/","%"];
+
+ //if functions confirmed look at related array and pull 
+ if (upperCharas) {
+
+ }
+  
+  
+  
+  
+  
+  
+  
+  
   passwordLength()
   if (upperCharas === false && lowerCharas === false && numeric === false && special === false) {
-    window.alert ("Your password cannot be generated. Try again.")
+    window.alert ("Your password cannot be generated. Try again.");
   }
 
 //function for password length
   function passwordLength () {
-  var passLength = window.prompt("Enter desired password length between 8 and 128 characters.")
+  var passLength = window.prompt("Enter desired password length between 8 and 128 characters.");
   if (passLength < 8 || passLength > 128 || passLength === "" || passLength === null) {
-    window.alert("Please choose a valid length.")
-    writePassword();
+    window.alert("Please choose a valid length.");
+    passwordLength();
   } else
-    console.log(passLength)
+    console.log(passLength);
     upperCharas();
 }
 
 //function to include uppercase characters
 function upperCharas () {
-  var uppercase = window.confirm ("Include upper case characters?")
+  var uppercase = window.confirm ("Include upper case characters?");
   if (uppercase) {
-    console.log("include uppercase characters")
-    upperCharas = true
+    console.log("include uppercase characters");
     lowerCharas();
   } else {
-    console.log("no uppercase characters")
-    lowerCharas ();
-    upperCharas = false
+    console.log("no uppercase characters");
+    lowerCharas();
   }
 }
 
 //function to include lowercase characters
 function lowerCharas () {
-  var lowercase = window.confirm ("Include lower case characters?")
+  var lowercase = window.confirm ("Include lower case characters?");
   if (lowercase) {
-    console.log("include lowercase characters")
-    lowerCharas = true
-    numeric ();
+    console.log("include lowercase characters");
+    numeric();
   } else {
-    console.log("no lower case characters")
-    numeric ();
-    lowerCharas = false
+    console.log("no lower case characters");
+    numeric();
   }
 }
 
 //function to include numbers
 function numeric () {
-  var numbers = window.confirm ("Include numbers?")
+  var numbers = window.confirm ("Include numbers?");
   if (numbers) {
-    console.log("include numbers")
-    numeric = true
-    special ();
+    console.log("include numbers");
+    special();
   } else {
-    console.log("no numbers")
-    special ();
-    numeric = false
+    console.log("no numbers");
+    special();
   }
 }
 
 //function to include special characters
 function special () {
-  var specialCharas = window.confirm ("Include special characters?")
+  var specialCharas = window.confirm ("Include special characters?");
   if (specialCharas) {
-    console.log("include special characters")
-    special = true
+    console.log("include special characters");
   } else {
-    console.log("no special characters")
-    special = false
+    console.log("no special characters");
   }
 }
 }
+
 
